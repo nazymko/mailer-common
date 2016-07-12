489e8e0a6233ffcff711e85c181e27c342a5f33f
anazymko <nazymko.patronus@gmail.com>
2016-07-12 12:18:17 +0300

    Initial commit

---
+++ src/org/nazymko/mailer/Message.java
@@ -0,0 +1,59 @@
+package org.nazymko.mailer;
+
+public class Message {
+    private String[] cc;
+    private String[] bcc;
+    private String subject;
+    private String from;
+    private String[] to;
+    private String message;
+
+    public String[] getCc() {
+        return this.cc;
+    }
+
+    public void setCc(String[] cc) {
+        this.cc = cc;
+    }
+
+    public String[] getBcc() {
+        return this.bcc;
+    }
+
+    public void setBcc(String[] bcc) {
+        this.bcc = bcc;
+    }
+
+    public String getSubject() {
+        return this.subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getFrom() {
+        return this.from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String[] getTo() {
+        return this.to;
+    }
+
+    public void setTo(String[] to) {
+        this.to = to;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
